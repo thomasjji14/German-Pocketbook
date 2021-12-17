f82bd4ba678b88cbbf30de975d69a5b774a63f9b
unknown <bankerice@gmail.com>
2021-12-16 19:59:58 -0500

    Rearranged interface slightly into a class. WIP

--- interface.py
+++ interface.py
@@ -3,65 +3,100 @@ from tkinter import *
 from frames import *
 from fileManager import getFile
 
-def dickFocus():
-    # Forces dickFrame on screen
-    translationFrame.grid_forget()
-    dictFrame.grid(row = 0, column = 0, sticky = NSEW)
+def main():
+    base = Tk()
 
-def deepFocus():
-    # Forces comparisonFrame on screen
-    dictFrame.grid_forget()
-    translationFrame.grid(row = 0, column = 0, sticky = NSEW)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("word", nargs = '?')
+    args = parser.parse_args()
+    word = args.word if args.word is not None else ""
 
-parser = argparse.ArgumentParser()
-parser.add_argument("word", nargs = '?')
-args = parser.parse_args()
-word = args.word if args.word is not None else ""
+    Pocketbook(base, word)
+    base.mainloop()
 
-base = Tk()
-base.title("German Pocketbook")
-base.geometry('600x300')
-base.minsize(600, 300)
+class Pocketbook:
+    def __init__(self, base, word):
+        self._base = base
 
-pageFrame = Frame(base, borderwidth = 0, highlightthickness = 0)
-pageFrame.pack(side = LEFT, expand = True, fill = BOTH)
+        self._base.title("German Pocketbook")
+        self._base.geometry('600x300')
+        self._base.minsize(600, 300)
 
-menuFrame = Frame(pageFrame, borderwidth = 0, highlightthickness = 0)
-menuFrame.pack(side = TOP, anchor = W)
+        self._pageFrame = Frame(base, borderwidth = 0, highlightthickness = 0)
+        self._pageFrame.pack(side = LEFT, expand = True, fill = BOTH)
 
-dictccIcon = PhotoImage(file = getFile("images/dictccIcon.png"))
-dickButton = Button(menuFrame, command = dickFocus, image = dictccIcon)
-dickButton.grid(column = 0, row = 0, sticky = W)
-deepIcon = PhotoImage(file = getFile("images/translatorIcon.png"))
-deepButton = Button(menuFrame, image = deepIcon, command = deepFocus)
-deepButton.grid(column = 1, row = 0, sticky = W)
+        self._menuFrame = Frame(
+            self._pageFrame,
+            borderwidth = 0,
+            highlightthickness = 0
+        )
+        self._menuFrame.pack(side = TOP, anchor = W)
 
-genericBottomFrame = Frame(pageFrame, borderwidth = 0, highlightthickness = 0)
-genericBottomFrame.pack(side = BOTTOM, expand = True, fill = BOTH)
-genericBottomFrame.grid_propagate(False)
-genericBottomFrame.grid_rowconfigure(0, weight = 1)
-genericBottomFrame.grid_columnconfigure(0, weight = 1)
+        self._dictccIcon = PhotoImage(file = getFile("images/dictccIcon.png"))
+        self._dickButton = Button(
+            self._menuFrame,
+            command = self._dickFocus,
+            image = self._dictccIcon
+        )
+        self._dickButton.grid(column = 0, row = 0, sticky = W)
+        self._deepIcon = PhotoImage(file = getFile(
+                                        "images/translatorIcon.png"))
+        self._deepButton = Button(
+            self._menuFrame,
+            image = self._deepIcon,
+            command = self._deepFocus
+        )
+        self._deepButton.grid(column = 1, row = 0, sticky = W)
 
-# Dict.cc
-dictFrame = dictionaryFrame(genericBottomFrame)
-dictFrame.grid(column = 0, row = 0, sticky = NSEW)
+        self._genericBottomFrame = Frame(
+            self._pageFrame,
+            borderwidth = 0,
+            highlightthickness = 0
+        )
+        self._genericBottomFrame.pack(
+            side = BOTTOM,
+            expand = True,
+            fill = BOTH
+        )
+        self._genericBottomFrame.grid_propagate(False)
+        self._genericBottomFrame.grid_rowconfigure(0, weight = 1)
+        self._genericBottomFrame.grid_columnconfigure(0, weight = 1)
 
-translationFrame = translatorFrame(genericBottomFrame)
-translationFrame.grid(column = 0, row = 0, sticky = NSEW)
+        # Dict.cc
+        self._dictFrame = dictionaryFrame(self._genericBottomFrame)
+        self._dictFrame.grid(column = 0, row = 0, sticky = NSEW)
 
-# approximate number of words (1 + numSpaces)
-# doesn't activate both at run for faster startup
-if word.strip().count(" ") <= 1 or len(word) < 3:
-    dictFrame.provideTranslation(word)
-    dickFocus()
-else:
-    translationFrame.provideTranslation(word)
-    deepFocus()
+        self._translationFrame = translatorFrame(self._genericBottomFrame)
+        self._translationFrame.grid(column = 0, row = 0, sticky = NSEW)
 
-# Keyboard shortcuts to switch tabs
-menuFrame.bind_all("<Control-Key-1>", lambda event: dickFocus())
-menuFrame.bind_all("<F1>", lambda event: dickFocus())
-menuFrame.bind_all("<Control-Key-2>", lambda event: deepFocus())
-menuFrame.bind_all("<F2>", lambda event: deepFocus())
+        # approximate number of words (1 + numSpaces)
+        # doesn't activate both at run for faster startup
+        if word.strip().count(" ") <= 1 or len(word) < 3:
+            self._dictFrame.provideTranslation(word)
+            self._dickFocus()
+        else:
+            self._translationFrame.provideTranslation(word)
+            self._deepFocus()
 
-base.mainloop()
+        # Keyboard shortcuts to switch tabs
+        self._menuFrame.bind_all("<Control-Key-1>",
+                                 lambda event: self._dickFocus())
+        self._menuFrame.bind_all("<F1>",
+                                 lambda event: self._dickFocus())
+        self._menuFrame.bind_all("<Control-Key-2>",
+                                 lambda event: self._deepFocus())
+        self._menuFrame.bind_all("<F2>",
+                                 lambda event: self._deepFocus())
+
+    def _dickFocus(self):
+        # Forces dickFrame on screen
+        self._translationFrame.grid_forget()
+        self._dictFrame.grid(row = 0, column = 0, sticky = NSEW)
+
+    def _deepFocus(self):
+        # Forces comparisonFrame on screen
+        self._dictFrame.grid_forget()
+        self._translationFrame.grid(row = 0, column = 0, sticky = NSEW)
+
+if __name__ == "__main__":
+    main()
